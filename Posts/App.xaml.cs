31526753b0a7a6e5ba29b2214ef7768ba185905d
Yelisei Melnichuk <eliseiml.ml@yandex.ru>
2021-09-23 13:17:03 +0300

    * .gitignore: Initial commit
    * Posts.sln:
    * App.xaml:
    * Main.cs:
    * App.xaml.cs:
    * Posts.csproj:
    * MainPage.xaml:
    * Info.plist:
    * FodyWeavers.xml:
    * AssemblyInfo.cs:
    * MainPage.xaml.cs:
    * NavigationBar.xaml:
    * AppDelegate.cs:
    * Posts.iOS.csproj:
    * NavigationBar.xaml.cs:
    * Entitlements.plist:
    * MainActivity.cs:
    * Default.png:
    * Posts.Android.csproj:
    * Default@2x.png:
    * AssemblyInfo.cs:
    * AboutAssets.txt:
    * Default-568h@2x.png:
    * AssemblyInfo.cs:
    * Default-Portrait.png:
    * styles.xml:
    * colors.xml:
    * AboutResources.txt:
    * LaunchScreen.storyboard:
    * Default-Portrait@2x.png:
    * Resource.designer.cs:
    * AndroidManifest.xml:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.xml:
    * Icon80.png:
    * Icon76.png:
    * Icon29.png:
    * Icon40.png:
    * Icon58.png:
    * Icon20.png:
    * Icon60.png:
    * Icon87.png:
    * Icon180.png:
    * Icon167.png:
    * Icon152.png:
    * icon_round.xml:
    * Icon120.png:
    * Icon1024.png:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * Posts.Android.csproj.AssemblyReference.cache:
    * Posts.iOS.csproj.AssemblyReference.cache:

---
+++ Posts/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Posts.Pages;
+using Posts.PageModels;
+using FreshMvvm;
+
+namespace Posts
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var postsPage = FreshPageModelResolver.ResolvePageModel<PostsPageModel>();
+            var postsContainer = new FreshNavigationContainer(postsPage, "Container");
+            MainPage = postsContainer;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Posts/NavigationBar.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace Posts
+{
+    public partial class NavigationBar
+    {
+        #region -- Public properties --
+
+
+        public static readonly BindableProperty LeftRightColumnDefinitionProperty = BindableProperty.Create(nameof(LeftRightColumnDefinition), typeof(GridLength), typeof(NavigationBar),
+            defaultValue: GridLength.Star);
+
+        public GridLength LeftRightColumnDefinition
+        {
+            get => (GridLength)GetValue(LeftRightColumnDefinitionProperty);
+            set => SetValue(LeftRightColumnDefinitionProperty, value);
+        }
+
+        public static readonly BindableProperty TitleStyleProperty = BindableProperty.Create(nameof(TitleStyle), typeof(Style), typeof(NavigationBar),
+            default(Style));
+
+        public Style TitleStyle
+        {
+            get => (Style)GetValue(TitleStyleProperty);
+            set => SetValue(TitleStyleProperty, value);
+        }
+
+        public static readonly BindableProperty TitleProperty =
+            BindableProperty.Create("Title", typeof(string), typeof(NavigationBar), string.Empty, BindingMode.TwoWay, propertyChanged: TitlePropertyChanged);
+
+
+        public IList<View> RightToolbarItems { get; set; }
+
+        public IList<View> LeftToolbarItems { get; set; }
+
+        public IList<View> CentralToolBarItems { get; set; }
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public NavigationBar()
+        {
+            InitializeComponent();
+
+            var rightToolbarItems = new ObservableCollection<View>();
+            rightToolbarItems.CollectionChanged += OnRightToolbarItemsCollectionChanged;
+            RightToolbarItems = rightToolbarItems;
+
+            var leftToolbarItems = new ObservableCollection<View>();
+            leftToolbarItems.CollectionChanged += OnLeftToolbarItemsCollectionChanged;
+            LeftToolbarItems = leftToolbarItems;
+
+            var centralToolbarItems = new ObservableCollection<View>();
+            centralToolbarItems.CollectionChanged += OnCentralToolbarItemsCollectionChanged;
+            CentralToolBarItems = centralToolbarItems;
+
+            Title = null;
+        }
+
+        #endregion
+
+        #region -- Private helpers --
+
+        private static void TitlePropertyChanged(BindableObject bindable, object oldvalue, object newvalue)
+        {
+            var newText = newvalue as String;
+            ((NavigationBar)bindable).TitleLabel.Text = newText;
+        }
+
+        private void OnRightToolbarItemsCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            RightToolBarItemsContainer.Children.Clear();
+            foreach (var toolbarItem in RightToolbarItems)
+            {
+                toolbarItem.BindingContext = BindingContext;
+                RightToolBarItemsContainer.Children.Add(toolbarItem);
+            }
+        }
+
+        private void OnLeftToolbarItemsCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            LeftToolBarItemsContainer.Children.Clear();
+            foreach (var toolbarItem in LeftToolbarItems)
+            {
+                toolbarItem.BindingContext = BindingContext;
+                LeftToolBarItemsContainer.Children.Add(toolbarItem);
+            }
+        }
+
+        private void OnCentralToolbarItemsCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            CentralToolBarItemsContainer.Children.Clear();
+
+            if (CentralToolBarItems.Any())
+            {
+                TitleLabel.IsVisible = false;
+
+                foreach (var toolbarItem in CentralToolBarItems)
+                {
+                    toolbarItem.BindingContext = BindingContext;
+                    CentralToolBarItemsContainer.Children.Add(toolbarItem);
+                }
+            }
+            else
+            {
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+
+        #endregion
+
+        #region -- Overrides --
+
+        protected override void OnPropertyChanged(string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+            if (propertyName == nameof(Title))
+            {
+                TitleLabel.IsVisible = !string.IsNullOrWhiteSpace(Title);
+                TitleLabel.Text = Title;
+            }
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            foreach (var toolbarItem in LeftToolbarItems)
+            {
+                toolbarItem.BindingContext = BindingContext;
+            }
+
+            foreach (var toolbarItem in RightToolbarItems)
+            {
+                toolbarItem.BindingContext = BindingContext;
+            }
+
+            foreach (var toolbarItem in CentralToolBarItems)
+            {
+                toolbarItem.BindingContext = BindingContext;
+            }
+        }
+
+        #endregion
+    }
+}
